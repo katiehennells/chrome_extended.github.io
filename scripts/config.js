e872d99f23b4863f0e472992b46b9ab88a74df9c
Katelyn Hennells <katelynhennells@Katies-MacBook-Pro.local>
2017-09-24 20:11:33 -0400

    init commit

---
+++ scripts/config.js
@@ -0,0 +1,30 @@
+var config = {
+	faces: {
+		//david: 'images/david_face.png',
+		bentley: 'images/Bentley.png'
+	},
+	prefixes: [
+		'Loving',
+		'Drunk',
+		'Chatty',
+		'Merry',
+		'Creepy',
+		'Introspecitve',
+		'Cheerful',
+		'Awkward',
+		'Unrelatable',
+		'Hungry'
+		],
+	bgColors: [
+		'#55d6be',
+		'#acecd9',
+		'#7d5ba6',
+		'#dddddd',
+		'#fc6471',
+		'#454ade',
+		'#1b1f3b',
+		'#b14aed',
+		'#c874d9'
+		],
+	customProps: {}
+};
